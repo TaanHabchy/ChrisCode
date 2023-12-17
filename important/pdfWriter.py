2e4cbd34ffad6e06b95a16e7e975572a569c2e3d
mjosephs04 <95541571+mjosephs04@users.noreply.github.com>
2023-12-16 21:26:26 -0500

    Update pdfWriter.py

--- important/pdfWriter.py
+++ important/pdfWriter.py
@@ -1,30 +1,37 @@
-from reportlab.lib.pagesizes import letter
+import os
 from reportlab.pdfgen import canvas
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
-import customHashish as hash
+import customHash as hash
+from tkinter import messagebox
 
 pageWidth = 600
 
 # registering fonts
-pdfmetrics.registerFont(TTFont('Marker', 'fonts/Bangers.ttf'))
-pdfmetrics.registerFont(TTFont('Tough', 'fonts/BlackOpsOne-Regular.ttf'))
-pdfmetrics.registerFont(TTFont('College', 'fonts/colleges.ttf'))
-pdfmetrics.registerFont(TTFont('Iceberg', 'fonts/Iceberg.ttf'))
-pdfmetrics.registerFont(TTFont('Roboto', 'fonts/Roboto-Bold.ttf'))
-pdfmetrics.registerFont(TTFont('Jersey', 'fonts/sportsjersey.ttf'))
-pdfmetrics.registerFont(TTFont('Asos', 'fonts/full_Pack_2025.ttf'))
-pdfmetrics.registerFont(TTFont('Adventure', 'fonts/SF Fedora.ttf'))
-pdfmetrics.registerFont(TTFont('Sporty', 'fonts/Calibri Bold.TTF'))
-pdfmetrics.registerFont(TTFont('Burny', 'fonts/Burny.ttf'))
-pdfmetrics.registerFont(TTFont('Rounded', "fonts/Dosis-Medium.ttf"))
+#pdfmetrics.registerFont(TTFont('Rounded', "Dosis-regular11.ttf"))
+
+try:
+    pdfmetrics.registerFont(TTFont('Burny', 'Burny.ttf'))
+    pdfmetrics.registerFont(TTFont('Marker', 'Bangers.ttf'))
+    pdfmetrics.registerFont(TTFont('Tough', 'BlackOpsOne-Regular.ttf'))
+    pdfmetrics.registerFont(TTFont('College', 'College.ttf'))
+    pdfmetrics.registerFont(TTFont('Iceberg', 'Iceberg.ttf'))
+    pdfmetrics.registerFont(TTFont('Roboto', 'Roboto-Bold.ttf'))
+    pdfmetrics.registerFont(TTFont('Jersey', 'sportsjersey.ttf'))
+    pdfmetrics.registerFont(TTFont('Asos', 'Full Pack 2025.ttf'))
+    pdfmetrics.registerFont(TTFont('Adventure', 'SF Fedora.ttf'))
+    pdfmetrics.registerFont(TTFont('Sporty', 'Calibri-bold.TTF'))
+    pdfmetrics.registerFont(TTFont('Rounded', "Dosis-regular.ttf"))
+except:
+    messagebox.showerror("Error", "Error: Cannot open a specific font")
 
 """
 Gives a relative size to each font
 """
+
 avgSize = 100
 fontSizeHash = {}
-fontSizeHash["Marker"] = avgSize
+fontSizeHash["Marker"] = avgSize + 15
 fontSizeHash["Tough"] = avgSize + 25
 fontSizeHash["College"] = avgSize + 35
 fontSizeHash["Iceberg"] = avgSize
@@ -47,9 +54,12 @@ def findSize(font):
 Creates a basic pdf with simple string written on it
 I use this to help with debugging to make sure I can reach the writing portion of the program
 """
-def tester(color):
-    c = canvas.Canvas(color + ".pdf", pagesize=letter)
-    c.drawString(20, 20, "helo")
+def tester():
+    c = canvas.Canvas(os.path.expanduser('~') + '/Desktop/test.pdf')
+    c.setPageSize((864, 1224))
+    # print(os.path.dirname(sys.executable))
+    print(os.path.expanduser('~'))
+    #c.drawString(20, 20, "helo")
     c.save()
 
 
@@ -60,30 +70,41 @@ Input: color of text
 
 def makePDF(color):
     currentWidth = 15  # the width that we will be writing at
-    drawHeight = 670  # top of pdf where we will start writing
+    drawHeight = 1300  # top of pdf where we will start writing
 
     if (len(hash.colorDataHash[color]) != 0):  # if there are orders with that specific color
-        c = canvas.Canvas(color + ".pdf", pagesize=letter)  # creating a pdf instance
+        c = canvas.Canvas(os.path.expanduser('~') + '/Desktop/PDFS/' + color + ".pdf")  # creating a pdf instance
+        c.setPageSize((590, 1404))
 
         for i in range(len(hash.colorDataHash[color])):
             customText = hash.colorDataHash[color][i] # for each iteration, customText is a different customized text
             for j in range(customText.quantity):
-                c.setFont(customText.font, findSize(customText.font))
+                fontSize = findSize(customText.font)
+                c.setFont(customText.font, fontSize)
                 stringWidth = pdfmetrics.stringWidth(customText.text, customText.font,
-                                             findSize(customText.font))  # gives the overall text width
+                                             fontSize)  # gives the overall text width
 
-                if currentWidth + stringWidth > pageWidth:  # if the string when fully written is wider than the page
+                print("String width: " + str(stringWidth) + "\nText: " + customText.text + "\nCurrentWidth: " + str(currentWidth) + '\n')
+
+                if currentWidth + stringWidth > 575:  # if the string when fully written is wider than the page
                     currentWidth = 15  # go back to the left side
                     drawHeight -= 105  # new line
 
                     if drawHeight <= 0:  # if we have filled the page
                         c.showPage()  # create new page
-                        drawHeight = 670  # start back up at the top
-                        c.setFont(customText.font, findSize(customText.font))  # need to reassign font, idk why but you do
+                        drawHeight = 1300  # start back up at the top
+                        c.setFont(customText.font, fontSize)  # need to reassign font, idk why but you do
 
                 c.drawString(currentWidth, drawHeight, customText.text)  # draws the string
-                currentWidth += stringWidth + 30  # adds space for next text input
-        c.save()
+
+                currentWidth += stringWidth + 10  # adds space for next text input
+        try:
+            c.scale(.5, .5)
+            c.save()
+        except:
+            messagebox.showerror("ERROR", "File could not save!")
+
+
 
 
 def makeAllPDFs():
